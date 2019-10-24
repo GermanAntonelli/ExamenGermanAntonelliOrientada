61a3a2d1afd6561a5d3ab2bec298254bcad56e69
German <German@DESKTOP-JDLKDN3>
2019-10-23 20:02:02 -0600

    creacion de todas las clases a utilizar en el proyecto

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,3 +1,20 @@
+import main.kotlin.Statetype
+import main.kotlin.classElevador
+
 fun main(args: Array<String>){
-println("Hello word")
+
+    var classelevadorObject = classElevador("detenido", "arriba","abajo", "primero", Statetype.Abajo)
+     classelevadorObject.showelevador()
+
+    val classElevadorObjectDetenido = classElevadorDetenido()
+    classElevadorObjectDetenido.showelevadorDetenido()
+
+    val classElevadorAbajoObject = classElevadorAbajo()
+    classElevadorAbajoObject.showelevadorabajo()
+
+    val classElevadorArrivaObject = classElevadorArriva()
+    //classElevadorAbajoObject.showelevadorarriba()
+
+
+
 }
